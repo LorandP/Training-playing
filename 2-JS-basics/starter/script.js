ace1f1d74777c4b3f57230201bf881214119bfc5
Palfavi Lorand <lorand.palfalvi@yopeso.com>
2020-03-08 19:28:51 +0200

    Add exercises

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,71 @@
+/***************
+* Mutations and coercions
+*
+
+console.log("Hellow World!");
+
+var firstName = "Jhon";
+var age = 28;
+
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "teacher";
+isMarried = false;
+
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " year old " +
+    job +
+    ". Is he married? " +
+    isMarried
+);
+var lastName = prompt("What is his last name?");
+*/
+
+
+
+/*******************
+ * Operators
+ *
+
+ var year, yearJhon, yearMark;
+ now = 2018;
+ ageJhon = 28;
+ ageMark = 33;
+
+ yearJhon = now - ageJhon;
+ yearMark = now - ageMark;
+
+ console.log(now + 2)
+ console.log(now * 2)
+ console.log(now / 10) 
+
+ var jhonOlder = ageJhon < ageMark;
+ console.log(jhonOlder)
+
+ console.log(typeof jhonOlder)
+ console.log(typeof ageJhon)
+ var x;
+ console.log(typeof x); // -> undefined
+ */
+/*
+function* foo(index) {
+  while (index < 2) {
+    yield index++;
+  }
+}
+
+const iterator = foo(0);
+iterator
+
+console.log(iterator.next().value);
+// expected output: 0
+
+console.log(iterator.next().value);
+// expected output: 1
+*/
+
+
